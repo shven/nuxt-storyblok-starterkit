31390bb5641976260f9cb31d612cdbac42ea0817
Sven Aarts <sven.aarts@telenetgroup.be>
2018-08-24 08:22:09 +0200

    add opacity to paragraphs and hover effect for links

--- components/RichText.vue
+++ components/RichText.vue
@@ -20,3 +20,35 @@
     },
   }
 </script>
+
+<style lang="scss">
+.Text {
+ p {
+   opacity: 0.8;
+ }
+
+ a {
+   position: relative;
+   display: inline-block;
+   font-weight: bold;
+   &:hover {
+     text-decoration: none;
+     &:after {
+       width: 80%;
+       will-change: width;
+     }
+   }
+   &:after {
+     content: ' ';
+     position: absolute;
+     bottom: 0;
+     left: 50%;
+     height: 2px;
+     background: currentColor;
+     width: 100%;
+     transition: width 0.15s ease-out;
+     transform: translateX(-50%);
+   }
+ }
+}
+</style>
--- plugins/lazyLoad.js
+++ plugins/lazyLoad.js
@@ -3,4 +3,6 @@
 import Vue from 'vue'
 import VueLazyload from 'vue-lazyload'
 
-Vue.use(VueLazyload)
+Vue.use(VueLazyload, {
+  preLoad: 1.75
+})
